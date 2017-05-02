b1b25a113e9e23d30054b231643e3f2c671db460
SZSilence06 <SZ_Silence06@foxmail.com>
2017-05-02 18:08:39 +0800

    Rearrange files.

---
+++ src/GUI-qt/CMakeFiles/GUI-qt.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zju/program/github/curve-design/src/GUI-qt/GUI-qt_automoc.cpp" "/home/zju/program/github/curve-design/src/GUI-qt/CMakeFiles/GUI-qt.dir/GUI-qt_automoc.cpp.o"
+  "/home/zju/program/github/curve-design/src/GUI-qt/main.cpp" "/home/zju/program/github/curve-design/src/GUI-qt/CMakeFiles/GUI-qt.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/zju/program/github/curve-design/src/qt_platform/CMakeFiles/qt_platform.dir/DependInfo.cmake"
+  "/home/zju/program/github/curve-design/src/app/CMakeFiles/app.dir/DependInfo.cmake"
+  "/home/zju/program/github/curve-design/src/UI_op/CMakeFiles/UI_op.dir/DependInfo.cmake"
+  "/home/zju/program/github/curve-design/src/alg/CMakeFiles/alg.dir/DependInfo.cmake"
+  "/home/zju/program/github/curve-design/src/working_space/CMakeFiles/working_space.dir/DependInfo.cmake"
+  "/home/zju/program/github/curve-design/src/working_space_render/CMakeFiles/working_space_render.dir/DependInfo.cmake"
+  "/home/zju/program/github/curve-design/src/globals/CMakeFiles/globals.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "src/GUI-qt"
+  "/home/zju/usr/include"
+  "/home/zju/usr/Linux/include"
+  "/home/zju/usr/Linux/64/include"
+  "/home/zju/usr/Linux/64/gcc4/include"
+  "src/common"
+  "3rd/include"
+  "/usr/include/qt5"
+  "/usr/include/qt5/QtWidgets"
+  "/usr/include/qt5/QtGui"
+  "/usr/include/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++-64"
+  "/usr/include/qt5/QtOpenGL"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
