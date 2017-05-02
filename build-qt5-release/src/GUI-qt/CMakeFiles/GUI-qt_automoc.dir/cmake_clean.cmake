e2f4acea5b0d8e8b72151007e37e6c05cd5d1c6c
SZSilence06 <SZ_Silence06@foxmail.com>
2017-05-02 18:18:00 +0800

    Clear files.

--- build-qt5-release/src/GUI-qt/CMakeFiles/GUI-qt_automoc.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "GUI-qt_automoc.cpp"
-  "CMakeFiles/GUI-qt_automoc"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/GUI-qt_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
