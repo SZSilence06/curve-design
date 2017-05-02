6a37072bded882284d55c0922f084929c9ce6b13
SZSilence06 <SZ_Silence06@foxmail.com>
2017-05-02 18:17:02 +0800

    Clear files.

---
+++ build-qt5-release/src/cmake_install.cmake
@@ -0,0 +1,47 @@
+# Install script for directory: /home/zju/program/github/curve-design/src
+
+# Set the install prefix
+IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  SET(CMAKE_INSTALL_PREFIX "/usr/local")
+ENDIF(NOT DEFINED CMAKE_INSTALL_PREFIX)
+STRING(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+IF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  IF(BUILD_TYPE)
+    STRING(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  ELSE(BUILD_TYPE)
+    SET(CMAKE_INSTALL_CONFIG_NAME "Release")
+  ENDIF(BUILD_TYPE)
+  MESSAGE(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+ENDIF(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+
+# Set the component getting installed.
+IF(NOT CMAKE_INSTALL_COMPONENT)
+  IF(COMPONENT)
+    MESSAGE(STATUS "Install component: \"${COMPONENT}\"")
+    SET(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  ELSE(COMPONENT)
+    SET(CMAKE_INSTALL_COMPONENT)
+  ENDIF(COMPONENT)
+ENDIF(NOT CMAKE_INSTALL_COMPONENT)
+
+# Install shared libraries without execute permission?
+IF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  SET(CMAKE_INSTALL_SO_NO_EXE "1")
+ENDIF(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+
+IF(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  INCLUDE("/home/zju/program/github/curve-design/build-qt5-release/src/app/cmake_install.cmake")
+  INCLUDE("/home/zju/program/github/curve-design/build-qt5-release/src/globals/cmake_install.cmake")
+  INCLUDE("/home/zju/program/github/curve-design/build-qt5-release/src/UI_op/cmake_install.cmake")
+  INCLUDE("/home/zju/program/github/curve-design/build-qt5-release/src/working_space/cmake_install.cmake")
+  INCLUDE("/home/zju/program/github/curve-design/build-qt5-release/src/working_space_render/cmake_install.cmake")
+  INCLUDE("/home/zju/program/github/curve-design/build-qt5-release/src/GUI-qt/cmake_install.cmake")
+  INCLUDE("/home/zju/program/github/curve-design/build-qt5-release/src/qt_platform/cmake_install.cmake")
+  INCLUDE("/home/zju/program/github/curve-design/build-qt5-release/src/alg/cmake_install.cmake")
+
+ENDIF(NOT CMAKE_INSTALL_LOCAL_ONLY)
+
--- src/GUI-qt/CMakeFiles/GUI-qt.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for GUI-qt.
-# This may be replaced when dependencies are built.
